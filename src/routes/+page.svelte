e9bbfbfeb71290f910fb85d42065b31ee2b974af
Donny C <chikwemdonald@gmail.com>
2025-12-25 22:38:59 +0100

    Add form functionality to capture name and message input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to MerryMessage</h1>
-<p>Create a christmas message to your loved ones</p>
+<script>
+    let name = $state("");
+    let message = $state("");
+    function onsubmit(e) {
+        e.preventDefault();
+
+        alert(`Name: ${name} \n Message: ${message}`)
+    }
+</script>
+
+<main>
+    <h1>Welcome to MerryMessage</h1>
+    <p>Create a christmas message to your loved ones</p>
+    
+    <form action="post" {onsubmit}>
+        <label for="name">Receiver's Name</label>
+        <input type="text" id="name" name="name" bind:value={name} />
+        <label for="message">Message:  </label>
+        <textarea name="message" id="message" bind:value={message}></textarea>
+        <button>Generate link</button>
+    </form>
+</main>
